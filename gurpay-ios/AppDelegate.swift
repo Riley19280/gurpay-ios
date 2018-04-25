1e155f3fe5af9be651dcc0d7ecd5a136709fd711
riley19280 <rschoppa@uncc.edu>
2018-04-25 11:38:02 -0400

    added pods

--- gurpay-ios/AppDelegate.swift
+++ gurpay-ios/AppDelegate.swift
@@ -16,6 +16,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let exampleViewController: UIViewController = mainStoryboard.instantiateInitialViewController()!
+        
+        self.window?.rootViewController = exampleViewController
+        
+        self.window?.makeKeyAndVisible()
+        
+        return true
+        
+        
         return true
     }
 
