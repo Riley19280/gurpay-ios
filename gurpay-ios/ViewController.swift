234fbc14c0152734609dfbd62212eb12586c3036
riley19280 <rschoppa@uncc.edu>
2018-04-25 10:39:12 -0400

    Initial Commit

---
+++ gurpay-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gurpay-ios
+//
+//  Created by Schoppa, Riley on 4/25/18.
+//  Copyright © 2018 Rileystech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gurpay-iosTests/gurpay_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  gurpay_iosTests.swift
+//  gurpay-iosTests
+//
+//  Created by Schoppa, Riley on 4/25/18.
+//  Copyright © 2018 Rileystech. All rights reserved.
+//
+
+import XCTest
+@testable import gurpay_ios
+
+class gurpay_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
